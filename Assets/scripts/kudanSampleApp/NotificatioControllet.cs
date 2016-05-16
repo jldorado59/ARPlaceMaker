5b18c94ca866743191e2978cafd4b28ea4225d77
josdor <jl.dorado59@uniandes.edu.co>
2016-05-16 04:39:10 -0500

    notificaciones de mentiras

---
+++ Assets/scripts/kudanSampleApp/NotificatioControllet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificatioControllet : MonoBehaviour {
+
+    public string[] Url;
+    public string[] Mensaje;
+
+	// Use this for initialization
+	void Start () {
+        this.StartCoroutine(Notificacion());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    IEnumerator Notificacion()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(5, 10));
+            int i = Random.Range(0, Url.Length);
+            UnityNotificationBar.UNotify("MENSAJE DEL DISTRITO!: " + Mensaje[i].ToUpper(), Url[i]);
+        }
+    }
+}
